1d6a4c7daa8bec9d1f7a5feace67e54a410878d9
Stuart MacKenzie <stu@ninjs.com>
2020-12-04 10:41:41 +0000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,58 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import {
+  getSettings,
+  getHero,
+  getAbout,
+  getServices,
+  getProjects,
+  getVideos
+} from "../lib/cms";
 
-export default function Home() {
+import Layout from "../components/Layout/Layout";
+import Hero from "../components/Hero/Hero";
+import About from "../components/About/About";
+import Services from "../components/Services/Services";
+import Projects from "../components/Projects/Projects";
+import Videos from "../components/Videos/Videos";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
+
+export default function Home({
+  settings,
+  hero,
+  about,
+  services,
+  projects,
+  videos
+}) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout settings={settings}>
+      <Hero item={hero} />
+      <About item={about} />
+      <Services items={services} />
+      <Projects items={projects} />
+      <Videos items={videos} />
+      <Contact settings={settings} />
+      <Footer settings={settings} />
+    </Layout>
+  );
+}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export async function getStaticProps() {
+  const settings = await getSettings();
+  const hero = await getHero();
+  const about = await getAbout();
+  const services = await getServices();
+  const projects = await getProjects();
+  const videos = await getVideos();
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  return {
+    props: {
+      settings,
+      hero,
+      about,
+      services,
+      projects,
+      videos
+    }
+  };
 }
