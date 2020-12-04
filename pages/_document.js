9c98b43ec463837c56de2817282d5a7a569b493e
Stuart MacKenzie <stu@ninjs.com>
2020-12-04 11:41:22 +0000

    added ga

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+import { GA_TRACKING_ID } from "../lib/gtag";
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -7,9 +9,21 @@ class MyDocument extends Document {
   }
 
   render() {
+    const gaSrc = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
+    const gaScript = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_TRACKING_ID}', {
+      page_path: window.location.pathname,
+    });
+  `;
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src={gaSrc} />
+          <script dangerouslySetInnerHTML={{ __html: gaScript }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
