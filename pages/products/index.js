9979192bb9b088d9791af6b9a489f489f04f9fd1
Stuart MacKenzie <stu@ninjs.com>
2022-01-19 13:13:11 -0500

    upgrade to next 12 / queue integration

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,10 +1,22 @@
+import Script from "next/script";
 import { getSettings, getProducts } from "@/lib/cms";
 import Layout from "@/components/Layout/Layout";
 import Products from "@/components/Products/Products";
 
+{
+  /* <script type='text/javascript' src='//static.queue-it.net/script/queueclient.min.js'></script>
+<script
+   data-queueit-c='ninjs'
+   type='text/javascript'
+   src='//static.queue-it.net/script/queueconfigloader.min.js'>
+</script> */
+}
+
 export default function ProductsPage({ settings, products }) {
   return (
     <Layout settings={settings}>
+      <Script src="https://static.queue-it.net/script/queueclient.min.js" />
+      <Script data-queueit-c="ninjs" src="https://static.queue-it.net/script/queueconfigloader.min.js" />
       <Products items={products} />
     </Layout>
   );
